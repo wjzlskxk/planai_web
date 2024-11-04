f9468154d790ad795c8f6f3cc7e0d3b0d1721101
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-11-05 08:31:16 +0900

    feat :: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import QueryClientProvider from "./components/common/provider/query-provier";
 import Router from "./components/router";
+import Header from "./components/common/header";
 
 function App() {
   return (
     <div className="App">
       <QueryClientProvider>
-        <Router />
+        {/* <Router /> */}
+        <Header />
       </QueryClientProvider>
     </div>
   );
---
+++ src/components/common/header/style.ts
@@ -0,0 +1,50 @@
+import { PlanaiColorToken } from "src/style/token/colorToken";
+import styled from "styled-components";
+import HeaderBackground from "src/assets/images/Header.png";
+
+export const HeaderWrap = styled.div`
+  width: calc(100vw - 20px);
+  height: 100px;
+  min-height: 80px;
+
+  background-image: url(${HeaderBackground});
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding-right: 20px;
+`;
+
+export const ButtonWrap = styled.div`
+  width: 10%;
+  height: 60%;
+
+  background-color: #f2f2f8;
+
+  border: none;
+  border-radius: 10px;
+
+  display: flex;
+  gap: 10px;
+
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 45px;
+    height: 45px;
+  }
+
+  span {
+    color: ${PlanaiColorToken.purple50};
+    font-family: Pretendard;
+    font-size: 28px;
+    font-weight: 500;
+    letter-spacing: 0.9px;
+  }
+
+  cursor: pointer;
+`;
