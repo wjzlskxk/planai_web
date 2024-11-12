8d462e9d936d65e0c0c5eb42bc2c3d8ff9b5b1b5
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-12 15:22:46 +0900

    chore :: deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import QueryClientProvider from "./components/common/provider/query-provier";
-import Router from "./components/router";
-import Header from "./components/common/header";
-import Sidebar from "./components/common/sidebar";
 import BottomBar from "./components/common/bottombar";
 
 function App() {
