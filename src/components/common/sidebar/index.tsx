6463bfcacb1235ba6f3f4ee9a062acf85f86ef00
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-12 18:15:32 +0900

    사이드바 들낙, 프로필 연결

--- src/components/common/sidebar/index.tsx
+++ src/components/common/sidebar/index.tsx
@@ -3,40 +3,47 @@ import * as S from './style';
 import { Home, HomeClicked, AI, AIClicked, News, NewsClicked, InButton } from 'src/assets/images/common/sidebar';
 import useSidebar from 'src/hooks/common/sidebar/useSIdebar';
 import { SIDEBAR_AI_ITEMS } from 'src/constants/common/sidebar/sidebar.constants';
+import Out from 'src/assets/images/outButton.png';
 
 const Sidebar = () => {
-  const { item, setItem, isVisible, setIsVisible, isClicked, handleIsClicked } = useSidebar();
+  const { item, setItem, isVisible, setIsVisible, isClicked, handleIsClicked, HandleCategoryClick } = useSidebar();
   return (
-    <S.SidebarWrap isvisible={isVisible}>
-      <S.SidebarItemWrap>
-        <S.SidebarItem iscliicked={item === '홈'} onClick={() => setItem('홈')}>
-          <img src={item !== '홈' ? Home : HomeClicked} alt="" />
-          <span>홈</span>
-        </S.SidebarItem>
-        <S.SidebarItem
-          iscliicked={item === '작성'}
-          onClick={() => {
-            setItem('작성');
-            handleIsClicked();
-          }}
-        >
-          <img src={item !== '작성' ? AI : AIClicked} alt="" />
-          <span>작성</span>
-        </S.SidebarItem>
-        {item === '작성' && isClicked === true && (
-          <S.SidebarWriteItem>
-            {SIDEBAR_AI_ITEMS.map((item, idx) => (
-              <div key={idx}>{item}</div>
-            ))}
-          </S.SidebarWriteItem>
-        )}
-        <S.SidebarItem iscliicked={item === '뉴스'} onClick={() => setItem('뉴스')}>
-          <img src={item !== '뉴스' ? News : NewsClicked} alt="" />
-          <span>뉴스</span>
-        </S.SidebarItem>
-      </S.SidebarItemWrap>
-      <S.InButton src={InButton} onClick={() => setIsVisible((prev) => !prev)} />
-    </S.SidebarWrap>
+    <>
+      {isVisible ? (
+        <S.SidebarWrap isvisible={isVisible}>
+          <S.SidebarItemWrap>
+            <S.SidebarItem iscliicked={item === '홈'} onClick={() => HandleCategoryClick('홈')}>
+              <img src={item !== '홈' ? Home : HomeClicked} alt="" />
+              <span>홈</span>
+            </S.SidebarItem>
+            <S.SidebarItem
+              iscliicked={item === '작성'}
+              onClick={() => {
+                HandleCategoryClick('작성');
+                handleIsClicked();
+              }}
+            >
+              <img src={item !== '작성' ? AI : AIClicked} alt="" />
+              <span>작성</span>
+            </S.SidebarItem>
+            {item === '작성' && isClicked === true && (
+              <S.SidebarWriteItem>
+                {SIDEBAR_AI_ITEMS.map((item, idx) => (
+                  <div key={idx}>{item}</div>
+                ))}
+              </S.SidebarWriteItem>
+            )}
+            <S.SidebarItem iscliicked={item !== '뉴스'} onClick={() => HandleCategoryClick('뉴스')}>
+              <img src={item === '뉴스' ? News : NewsClicked} alt="" />
+              <span>뉴스</span>
+            </S.SidebarItem>
+          </S.SidebarItemWrap>
+          <S.InButton src={InButton} onClick={() => setIsVisible((prev) => !prev)} />
+        </S.SidebarWrap>
+      ) : (
+        <img src={Out} onClick={() => setIsVisible((prev) => !prev)} />
+      )}
+    </>
   );
 };
 
--- src/hooks/common/sidebar/useSIdebar.ts
+++ src/hooks/common/sidebar/useSIdebar.ts
@@ -1,6 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { NavigationProps } from 'src/types/common/sidebar/sidebar.type';
 
 const useSidebar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [item, setItem] = useState<string>('홈');
   const [isClicked, setIsClicked] = useState<boolean>(false);
   const [isVisible, setIsVisible] = useState<boolean>(true);
@@ -9,13 +13,51 @@ const useSidebar = () => {
     setIsClicked((prev) => !prev);
   };
 
+  const HandleNavigation = ({ location, navigate }: NavigationProps) => {
+    switch (location.pathname) {
+      case '/':
+        setItem('홈');
+        break;
+      case '/write':
+        setItem('작성');
+        break;
+      case '/news':
+        setItem('뉴스');
+      default:
+        setItem('');
+        break;
+    }
+  };
+
+  const HandleCategoryClick = (itemName: string) => {
+    setItem(itemName);
+    switch (itemName) {
+      case '홈':
+        navigate('/');
+        break;
+      case '작성':
+        navigate('/write');
+        break;
+      case '뉴스':
+        navigate('/news');
+        break;
+      default:
+        navigate('');
+    }
+  };
+
+  useEffect(() => {
+    HandleNavigation({ location, navigate });
+  }, [location.pathname]);
+
   return {
     item,
     isVisible,
     isClicked,
     setItem,
     setIsVisible,
-    handleIsClicked
+    handleIsClicked,
+    HandleCategoryClick
   };
 };
 
